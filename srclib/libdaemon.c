9dc34efd7ceb1a94e2355a5d3596f5a29374256f
Tormenta74 <diego.sainzdemedrano@gmail.com>
2018-02-14 22:20:08 +0100

    Formatting. For the love of God.

--- srclib/libdaemon.c
+++ srclib/libdaemon.c
@@ -11,28 +11,29 @@
 int daemonize(const char* proc_name) {
     pid_t pid = fork();
 
-    if(pid > 0) exit(OK);
-    if(pid < 0) exit(ERR);
+    if(pid > 0) { exit(OK); }
+    if(pid < 0) { exit(ERR); }
 
     umask(0);
     setlogmask(LOG_UPTO(LOG_INFO));
-    openlog(proc_name, LOG_CONS|LOG_PID|LOG_NDELAY,LOG_LOCAL3);
-    syslog(LOG_ERR,"Sending server to background.");
+    openlog(proc_name, LOG_CONS|LOG_PID|LOG_NDELAY, LOG_LOCAL3);
+    syslog(LOG_ERR, "Sending server to background.");
 
     if(setsid() < 0) {
-        syslog(LOG_ERR,"Error creating session for the server.");
+        syslog(LOG_ERR, "Error creating session for the server.");
         return ERR;
     }
 
     if((chdir("/")) < 0) {
-        syslog(LOG_ERR,"Error changing directory to \"/\".");
+        syslog(LOG_ERR, "Error changing directory to \"/\".");
         return ERR;
     }
 
     syslog(LOG_INFO, "Closing all file descriptors.");
     int i, ts=getdtablesize();
-    for(i=0;i<ts;i++) 
+    for(i=0; i<ts; i++) {
         close(i);
+    }
 
     return OK;
 }
