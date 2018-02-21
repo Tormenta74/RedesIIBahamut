4eae8afabb33662048cfb3ca7104f5876037f088
Tormenta74 <diego.sainzdemedrano@gmail.com>
2018-02-21 16:48:34 +0100

    Added more configuration options. Fixed error in daemonize call. Fixed little mistake in config module (was returning ERR when not needed).

--- srclib/libdaemon.c
+++ srclib/libdaemon.c
@@ -17,19 +17,19 @@ int daemonize(const char *proc_name, const char *root_path) {
     umask(0);
     setlogmask(LOG_UPTO(LOG_INFO));
     openlog(proc_name, LOG_CONS|LOG_PID|LOG_NDELAY, LOG_LOCAL3);
-    syslog(LOG_ERR, "Sending server to background.");
+    print("Sending server to background.");
 
     if(setsid() < 0) {
-        syslog(LOG_ERR, "Error creating session for the server.");
+        print("Error creating session for the server.");
         return ERR;
     }
 
     if((chdir(root_path)) < 0) {
-        syslog(LOG_ERR, "Error changing directory to \"%s\".", root_path);
+        print("Error changing directory to \"%s\".", root_path);
         return ERR;
     }
 
-    syslog(LOG_INFO, "Closing all file descriptors.");
+    print("Closing all file descriptors.");
     int i, ts=getdtablesize();
     for(i=0; i<ts; i++) {
         close(i);
