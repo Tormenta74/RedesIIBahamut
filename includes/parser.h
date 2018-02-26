a1993c0002c9133c1bc254b4abcb1ae1db581e15
Tormenta74 <diego.sainzdemedrano@gmail.com>
2018-02-26 12:19:45 +0100

    Formatting in the parser module. 3 spaces is the mind killer.

--- includes/parser.h
+++ includes/parser.h
@@ -2,10 +2,11 @@
 #define _PARSER_H
 
 #define MAX_HEADERS 100
+#define MAX_LEN_HEADER 2048
 
 typedef struct request_headers {
-   char name[2048];
-   char value[2048];
+    char name[MAX_LEN_HEADER];
+    char value[MAX_LEN_HEADER];
 } request_headers_t;
 
 int request_parser(char *buf, size_t buflen, char *method, char *path, int *version, struct request_headers *headers, int *num_headers);
