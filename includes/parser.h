4b1b71d23161aef11dba7a190ddd245211204cc1
Tormenta74 <diego.sainzdemedrano@gmail.com>
2018-02-26 12:28:17 +0100

    Keep oooooon fixin'.

--- includes/parser.h
+++ includes/parser.h
@@ -4,11 +4,10 @@
 #define MAX_HEADERS 100
 
 typedef struct request_headers {
-   char *name;
-   char *value;
-   int num_headers;
+   char name[2048];
+   char value[2048];
 } request_headers_t;
 
-int request_parser(const char *buf, int buflen, char **method, char **path, int *version, struct request_headers *headers);
+int request_parser(char *buf, size_t buflen, char *method, char *path, int *version, struct request_headers *headers, int *num_headers);
 
 #endif
