6401c420be89e5ec787fa452be4bfc90aeca697f
Tormenta74 <diego.sainzdemedrano@gmail.com>
2018-03-01 12:01:42 +0100

    Added a whole bunch of commentary (and sweet formatting, delicious)

--- srclib/libconcurrent.c
+++ srclib/libconcurrent.c
@@ -5,35 +5,88 @@
 
 // conc = concurrent / concurrency
 
-/* routine es una función con signature igual que attention_routine,
- * es decir, compatible con los hilos de pthread */
-int conc_launch(void*(*routine)(void*), void* arg) {
+/*
+ * Description: Creates a thread executing the attention routine with its args.
+ *
+ * In:
+ * void*(*routine)(void*): a function to the pointer to be executed
+ * (see includes/server.h:attention_routine definition)
+ * void *arg: generic pointer to whatever argument the function needs
+ *
+ * Return: ERR in case of sanity failure. pthread_create's return otherwise.
+ */
+int conc_launch(void*(*routine)(void*), void *arg) {
     pthread_t tid;
-    if(routine) return pthread_create(&tid, NULL, routine, arg);
+    if(routine) {
+        return pthread_create(&tid, NULL, routine, arg);
+    }
     return ERR;
 }
 
+/*
+ * Description: Calls the thread-exit function of pthread
+ */
 void conc_exit() {
     pthread_exit(NULL);
 }
 
+/*
+ * Description: Wrapper around the mutex init function of pthread.
+ *
+ * In:
+ * pthread_mutex_t *lock: the mutex to be initialized
+ *
+ * Return: ERR in case of sanity failure. pthread_mutex_init's return otherwise.
+ */
 int mutex_init(pthread_mutex_t *lock) {
-    if(lock) return pthread_mutex_init(lock,NULL);
+    if(lock) {
+        return pthread_mutex_init(lock, NULL);
+    }
     return ERR;
 }
 
+/*
+ * Description: Wrapper around the lock function of pthread
+ *
+ * In:
+ * pthread_mutex_t *lock: the mutex to be locked
+ *
+ * Return: ERR in case of sanity failure. pthread_mutex_lock's return otherwise.
+ */
 int mutex_lock(pthread_mutex_t *lock) {
-    if(lock) return pthread_mutex_lock(lock);
+    if(lock) {
+        return pthread_mutex_lock(lock);
+    }
     return ERR;
 }
 
+/*
+ * Description: Wrapper around the unlock function of pthread
+ *
+ * In:
+ * pthread_mutex_t *lock: the mutex to be unlocked
+ *
+ * Return: ERR in case of sanity failure. pthread_mutex_unlock's return otherwise.
+ */
 int mutex_unlock(pthread_mutex_t *lock) {
-    if(lock) return pthread_mutex_unlock(lock);
+    if(lock) {
+        return pthread_mutex_unlock(lock);
+    }
     return ERR;
 }
 
+/*
+ * Description: Wrapper around the mutex destroy function of pthread
+ *
+ * In:
+ * pthread_mutex_t *lock: the mutex to be unlocked
+ *
+ * Return: ERR in case of sanity failure. pthread_mutex_destroy's return otherwise.
+ */
 int mutex_destroy(pthread_mutex_t *lock) {
-    if(lock) return pthread_mutex_destroy(lock);
+    if(lock) {
+        return pthread_mutex_destroy(lock);
+    }
     return ERR;
 }
 
--- srclib/libserver.c
+++ srclib/libserver.c
@@ -22,14 +22,25 @@ int n_conn;                     // número de clientes conectados
 int iter = 0;                   // bandera de modo iterativo
 fd_set active_set, read_set;    // set de sockets y set de control
 
-int server_setup_woptions(struct server_options *so) {
+/*
+ * Description: Establishes options for the server and sets the master socket.
+ *
+ * In:
+ * struct server_options *so: struct containing the loaded options
+ * (see includes/config.h for details)
+ *
+ * Return: ERR in case of failure at any point. OK otherwise.
+ */
+int server_setup(struct server_options *so) {
     int enable = 1;
 
+    // sanity check
     if(!so) {
         print("Unallocated server_options.");
         return ERR;
     }
 
+    // daemon mode option
     if(so->daemon == 1) {
         if(daemonize(so->server_signature, so->server_root) == ERR) {
             print("Bad daemonize.");
@@ -37,7 +48,7 @@ int server_setup_woptions(struct server_options *so) {
         }
     }
 
-    /* abrimos el socket de conexiones */
+    // open the master (connection) socket
     conn_socket = tcp_open_socket();
     if(conn_socket == ERR) {
         print("Could not open TCP socket (%s:%d).", __FILE__, __LINE__);
@@ -46,6 +57,7 @@ int server_setup_woptions(struct server_options *so) {
     }
     print("Connection socket opened with file descriptor %d.\n", conn_socket);
 
+    // apply socket options
     if(setsockopt(conn_socket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int))) {
         print("Could not set socket options (%s:%d).", __FILE__, __LINE__);
         print("errno (setsockopt): %s.", strerror(errno));
@@ -53,10 +65,10 @@ int server_setup_woptions(struct server_options *so) {
     }
     print("Socket options set (SO_REUSEADDR).");
 
-    /* mostramos los datos de nuestro servidor */
+    // relevant information to the testers
     print("Connection port: %d.", so->listen_port);
 
-    /* bind */
+    // bind the master socket to the listen port
     if(tcp_bind_port(conn_socket, INADDR_ANY, so->listen_port)) {
         print("Could not bind address to port (%s:%d).",__FILE__,__LINE__);
         print("errno (bind): %s.", strerror(errno));
@@ -64,13 +76,14 @@ int server_setup_woptions(struct server_options *so) {
     }
     print("Conection port bound successfully.");
 
-    /* escucha */
+    // mark socket as ready to accept new connections
     if(tcp_listen(conn_socket, so->max_clients)) {
         print("Could not set socket to listen (%s:%d).", __FILE__, __LINE__);
         print("errno (listen): %s.", strerror(errno));
         return ERR;
     }
 
+    // init number of connections variable lock
     if(mutex_init(&nconn_lock) == ERR) {
         print("Could not init mutex (%s:%d).", __FILE__, __LINE__);
         print("errno (pthread_mutex_init): %s.", strerror(errno));
@@ -78,13 +91,87 @@ int server_setup_woptions(struct server_options *so) {
     }
 
     print("Now listening.");
+
+    // activate the server
     active = 1;
+
+    // option to either launch threads for each client or execute iteratively
     iter = so->iterative;
+
     return OK;
 }
 
-/* inicia el modo demonio, abre el socket de conexión llama a bind y a listen */
-int server_setup(const char* servername, uint32_t local_addr, uint16_t local_port) {
+/*
+ * Description: While the "active" global variable is set to 1, accepts new
+ *              connections and launches threads to handle each client.
+ *
+ * In:
+ * attention_routine *fn: pointer to a function that handles a client connection
+ * (see includes/server.h for details)
+ *
+ * Return: ERR in case of failure at any point. OK otherwise.
+ */
+int server_accept_loop(attention_routine *fn) {
+    int new_socket;
+    uint8_t *reader;
+    struct sockaddr_in addr;
+
+
+    while(active) {
+        // inicializamos new_socket
+        new_socket = 0;
+
+        // inicializamos / limpiamos los campos de addr
+        addr.sin_family = 0;
+        addr.sin_port = 0;
+        addr.sin_addr.s_addr = 0;
+        bzero(addr.sin_zero, 8*sizeof(char));
+
+        if(tcp_accept(conn_socket, &new_socket, &addr) || new_socket == ERR) {
+            print("Could not accept conection request (%s:%d).", __FILE__, __LINE__);
+            print("errno (accept): %s.", strerror(errno));
+            active=0;
+            return ERR;
+        }
+
+        // leemos los datos de la nueva conexión
+        reader = (uint8_t*)&(addr.sin_addr.s_addr);
+        print("New connection: address = %d.%d.%d.%d", reader[0], reader[1], reader[2], reader[3]);
+        print("New connection: port = %d.", addr.sin_port);
+        print("Conection accepted: redirected to socket %d.", new_socket);
+
+
+        // lanzamos el hilo de atención, pasándole el número del socket
+        int* s = malloc(sizeof(int));
+        *s = new_socket;
+
+        mutex_lock(&nconn_lock);
+        if(++n_conn <= MAX_CLIENTS) {
+            if(iter == 1) { // el servidor tiene la opción de ser iterativo
+                // llamamos directamente a la rutina de atención
+                fn((void*)s);
+            } else {
+                // lanzamos concurrentemente la rutina de atención
+                conc_launch(fn, (void*)s);
+            }
+        } else {
+            // TODO: either nothing, or send a message to the client informing them
+            // that we are not accepting new connections
+            tcp_close_socket(new_socket);
+            free(s);
+        }
+        mutex_unlock(&nconn_lock);
+    }
+
+    return OK;
+}
+
+
+/************************************************
+ * DEPRECATED
+ */
+
+int server_setup_old(const char* servername, uint32_t local_addr, uint16_t local_port) {
     uint8_t *reader; // usado para leer correctamente la dirección IP
     int enable = 1;
 
@@ -137,65 +224,6 @@ int server_setup(const char* servername, uint32_t local_addr, uint16_t local_por
     return OK;
 }
 
-/* mientras "active" sea 1, el servidor se mantiene *
- * en un bucle de aceptar nuevas conexiones y       *
- * despachando hilos para atenderlas                */
-int server_accept_loop(attention_routine *fn) {
-    int new_socket;
-    uint8_t *reader;
-    struct sockaddr_in addr;
-
-
-    while(active) {
-        // inicializamos new_socket
-        new_socket = 0;
-
-        // inicializamos / limpiamos los campos de addr
-        addr.sin_family = 0;
-        addr.sin_port = 0;
-        addr.sin_addr.s_addr = 0;
-        bzero(addr.sin_zero, 8*sizeof(char));
-
-        if(tcp_accept(conn_socket, &new_socket, &addr) || new_socket == ERR) {
-            print("Could not accept conection request (%s:%d).", __FILE__, __LINE__);
-            print("errno (accept): %s.", strerror(errno));
-            active=0;
-            return ERR;
-        }
-
-        // leemos los datos de la nueva conexión
-        reader = (uint8_t*)&(addr.sin_addr.s_addr);
-        print("New connection: address = %d.%d.%d.%d", reader[0], reader[1], reader[2], reader[3]);
-        print("New connection: port = %d.", addr.sin_port);
-        print("Conection accepted: redirected to socket %d.", new_socket);
-
-
-        // lanzamos el hilo de atención, pasándole el número del socket
-        int* s = malloc(sizeof(int));
-        *s = new_socket;
-
-        mutex_lock(&nconn_lock);
-        if(++n_conn <= MAX_CLIENTS) {
-            if(iter == 1) { // el servidor tiene la opción de ser iterativo
-                // llamamos directamente a la rutina de atención
-                fn((void*)s);
-            } else {
-                // lanzamos concurrentemente la rutina de atención
-                conc_launch(fn, (void*)s);
-            }
-        } else {
-            // TODO: either nothing, or send a message to the client informing them
-            // that we are not accepting new connections
-        }
-        mutex_unlock(&nconn_lock);
-    }
-
-    return OK;
-}
-
-/* mientras "active" sea 1, el servidor se mantiene *
- * en un bucle de aceptar nuevas conexiones y       *
- * despachando hilos para atenderlas                */
 int server_accept_loop_old(attention_routine *fn) {
     int i, new_socket;
     uint8_t *reader;
@@ -259,3 +287,4 @@ int server_accept_loop_old(attention_routine *fn) {
 
     return OK;
 }
+
