e64ab37858de328dbadaff7e57e92b4137497e29
Fernando Villar Gómez <fernando.villarg@estudiante.uam.es>
2018-03-03 19:22:48 +0100

    headers.c and http.c commented

--- src/http.c
+++ src/http.c
@@ -10,8 +10,14 @@
 
 /****************************************************************
  * DATA STRUCTURES UTILS
- */
+ ****************************************************************/
 
+/*
+ * Description: Prints the information of a request by a client
+ *
+ * In:
+ * struct http_req_data *rd: structure which contains information on the request
+ * */
 void http_request_data_print(struct http_req_data *rd) {
     int i = 0;
 
@@ -24,11 +30,19 @@ void http_request_data_print(struct http_req_data *rd) {
         print("%s:\t%s", rd->headers[i].name, rd->headers[i].value);
     }
 
+    /* prints the body only if it exists (it doesn't exist if rd->body is NULL) */
     if(rd->body) {
         print("Body: %s", rd->body);
     }
 }
 
+/*
+ * Description: Frees the resources allocated to create a http_req_data structure which
+ * contains the information on a request by a client
+ *
+ * In:
+ * struct http_req_data *rd: structure to be freed which contains information on the request
+ * */
 void http_request_data_free(struct http_req_data *rd) {
     if(rd->method) {
         free(rd->method);
@@ -43,11 +57,22 @@ void http_request_data_free(struct http_req_data *rd) {
 
 /****************************************************************
  * PARSING REQUESTS AND RESPONSES
- */
+ ****************************************************************/
 
 
-/* function to set body pointer, sets NULL if there is no body.
- */
+ /*
+  * Description: aims to get a pointer to a string containing the body of a request.
+  * In some cases there may not be a body, if that happens we indicate the lack of body
+  * by the use of a NULL pointer; but, on the other hand, if the body exists, we provide
+  * the pointer to the required string.
+  *
+  * In:
+  * char *buf: buffer containing the request in which to look for the body
+  * char **body: pointer to string, used to return the pointer to the body
+  *
+  * Return:
+  * ERR if there has been an error during the process, OK otherwise
+  * */
 int http_response_body(char *buf, char **body) {
     char sequence[5], *pointer;
     int i = 0;
@@ -56,6 +81,7 @@ int http_response_body(char *buf, char **body) {
         return ERR;
     }
 
+    /* increases the pointer starting at buf[0] until it finds the sequence "\r\n\r\n" or the end of string '\0' */
     pointer = buf;
     do {
         sprintf(sequence, "%.*s", 4, pointer);
@@ -63,12 +89,16 @@ int http_response_body(char *buf, char **body) {
         i++;
     } while(strcmp(sequence, "\r\n\r\n")!=0 && *pointer!='\0');
 
+    /* if the loop has been exited due to finding the character '\0', the request was not built correctly and ERR is returned */
     if (*pointer == '\0') {
         return ERR;
     }
 
+    /* if the request was built correctly and "\r\n\r\n" has been found, we place the pointer at the beginning of the body */
     pointer += 3;
 
+    /* if the beginning of the body is actually the '\0' character, it means that there is no body and NULL is placed on *body;
+    otherwise, the pointer to the beginning of the body is placed on *body */
     if(*pointer != '\0') {
         *body = pointer;
     } else {
@@ -78,7 +108,23 @@ int http_response_body(char *buf, char **body) {
     return OK;
 }
 
-/* caller must free memory */
+/*
+ * Description: splits the path given by http_request_parse into two strings: the actual path
+ * and the arguments. Splitting is necessary only to respond to GET requests, and this case is
+ * identified by the presence of a '?' symbol (the symbol that separates both strings). If it
+ * is a GET request, the function allocates memory for both strings, and the caller has the
+ * responsibility to free them; if it is not a GET request, there is no allocation.
+ *
+ * In:
+ * char *buf: buffer that contains the path given by http_request_parse
+ * size_t buflen: length of buf
+ * char **path: pointer to string, to save the actual path
+ * char **args: pointer to string, to save the arguments
+ * size_t *args_len: pointer to size_t, to save the length of args
+ *
+ * Return:
+ * ERR if there has been an error during the process, OK otherwise
+ * */
 int http_request_get_split(char *buf, size_t buflen, char **path, char **args, size_t *args_len) {
     char *aux;
     size_t path_len_aux, args_len_aux;
@@ -87,27 +133,34 @@ int http_request_get_split(char *buf, size_t buflen, char **path, char **args, s
         return ERR;
     }
 
+    /* increases the pointer starting at buf[0] until it reaches either a '?' or a '\0' */
     aux = buf;
     while(*aux != '?' && *aux != '\0') {
         aux++;
     }
+    /* if it stopped at '\0', there is no '?', so there are no arguments and the given path is the actual path */
     if (*aux == '\0') {
+        /* we define the lack of arguments as a NULL pointer and size=0 */
         *args = NULL;
         *args_len = 0;
+        /* no allocation */
         *path = buf;
         return OK;
     }
 
+    /* sets length for both strings */
     path_len_aux = aux - buf;
     aux++;
     args_len_aux = buflen - (aux - buf);
 
+    /* memory allocation */
     *path = (char*)malloc(sizeof(char)*path_len_aux);
     *args = (char*)malloc(sizeof(char)*args_len_aux);
     if(!*path || !*args) {
         return ERR;
     }
 
+    /* sets actual path and arguments */
     sprintf(*path, "%.*s", (int)path_len_aux, buf);
     sprintf(*args, "%.*s", (int)args_len_aux, aux);
     *args_len = args_len_aux;
@@ -115,7 +168,18 @@ int http_request_get_split(char *buf, size_t buflen, char **path, char **args, s
     return OK;
 }
 
-/* wraps phr_parse_request and returns required information in a simple way */
+/*
+ * Description: parses a request, filling a structure with all the necessary information
+ * for the server to process and create a response.
+ *
+ * In:
+ * char *buf: buffer that contains the request
+ * size_t buflen: length of buf
+ * struct http_req_data *rd: structure in which to save all the information
+ *
+ * Return:
+ * ERR if there has been an error during the process, OK otherwise
+ * */
 int http_request_parse(char *buf, size_t buflen, struct http_req_data *rd) {
     char *method_aux, *path_aux, *body_aux;
     size_t method_len, path_len, body_len, nheaders_aux;
@@ -125,6 +189,7 @@ int http_request_parse(char *buf, size_t buflen, struct http_req_data *rd) {
     /* setting nheaders_aux to MAX_HEADERS */
     nheaders_aux = (sizeof(headers_aux) / sizeof(headers_aux[0]));
 
+    /* usage of the picohttpparser library function */
     ret = phr_parse_request(buf, buflen, (const char**)&method_aux, &method_len, (const char**)&path_aux, &path_len, &minor_version, headers_aux, &nheaders_aux, 0);
 
     if(ret <= 0) {
@@ -132,8 +197,7 @@ int http_request_parse(char *buf, size_t buflen, struct http_req_data *rd) {
         return ERR;
     }
 
-    /* set method, path, http version and number of headers */
-
+    /* sets method, path, http version and number of headers */
     rd->method = (char*)malloc(method_len*sizeof(char));
     if(!rd->method) {
         print("Failed to allocate memory (%s,%d)", __FILE__, __LINE__);
@@ -149,6 +213,10 @@ int http_request_parse(char *buf, size_t buflen, struct http_req_data *rd) {
     rd->path_len = path_len;
     sprintf(rd->path, "%.*s", (int)path_len, path_aux);
 
+    rd->version = minor_version;
+    rd->num_headers = nheaders_aux;
+
+    /* sets body, only if it exists; if it doesn't exist, rd->body is set to NULL and rd->body_len to 0 */
     ret = http_response_body(buf, &body_aux);
     if(ret == ERR) {
         print("http_response_body failure.\n");
@@ -158,6 +226,7 @@ int http_request_parse(char *buf, size_t buflen, struct http_req_data *rd) {
         rd->body = NULL;
         rd->body_len = 0;
     } else {
+        /* length of body calculation as (length of request) - (pointer at body - pointer at start of request) */
         body_len = buflen - (body_aux - buf);
         rd->body = (char*)malloc(body_len*sizeof(char));
         rd->body_len = body_len;
@@ -168,15 +237,8 @@ int http_request_parse(char *buf, size_t buflen, struct http_req_data *rd) {
         sprintf(rd->body, "%.*s", (int)body_len, body_aux);
     }
 
-    rd->version = minor_version;
+    /* fills http_pairs structure with pairs {name, value} and sets number of headers */
     rd->num_headers = nheaders_aux;
-
-    /*                                                       */
-    /* fills http_pairs structure with pairs {name, value} */
-    /*                                                       */
-
-    rd->num_headers = nheaders_aux;
-
     for(i=0; i<nheaders_aux; i++) {
         sprintf(rd->headers[i].name, "%.*s", (int)headers_aux[i].name_len, headers_aux[i].name);
         sprintf(rd->headers[i].value, "%.*s", (int)headers_aux[i].value_len, headers_aux[i].value);
@@ -185,7 +247,23 @@ int http_request_parse(char *buf, size_t buflen, struct http_req_data *rd) {
     return OK;
 }
 
-/* builds server responses */
+/*
+ * Description: Builds a server response (always after processing a request)
+ *
+ * In:
+ * char* buffer: string in which to save output response
+ * int version: version of HTTP used by the server
+ * int rescode: response code used by the server
+ * char *resp: string which contains the message associated to rescode
+ * size_t resp_len: length of resp
+ * int num_headers: number of headers of the response
+ * struct http_pairs *headers: structure cointaining all the information about headers
+ * char *body: body of the response
+ * size_t body_len: length of body
+ *
+ * Return:
+ * ERR if there has been an error during the process, OK otherwise
+ * */
 int http_response_build(char* buffer, int version, int rescode, char *resp, size_t resp_len, int num_headers, struct http_pairs *headers, char *body, size_t body_len) {
     int i;
     char buf[MAX_CHAR], buf_aux[MAX_CHAR];
@@ -214,8 +292,9 @@ int http_response_build(char* buffer, int version, int rescode, char *resp, size
     return OK;
 }
 
-/****************************************************************/
-/* DEPRECATED OR UNUSED */
+/****************************************************************
+ * DEPRECATED OR UNUSED
+ ***************************************************************/
 
 /* wraps phr_parse_response and returns required information in a simple way */
 int response_parser(char *buf, size_t buflen, int *version, int *rescode, char *resp, struct http_pairs *headers, int *num_headers) {
